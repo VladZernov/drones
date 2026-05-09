fd71f1ef4c29e80f16bc0a7da7015b7bb311f59f
Vlad Zernov <vlad.zernov@yahoo.com>
2026-05-09 16:02:39 +0200

    initial

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
 import type { NextConfig } from "next";
-import withPWAInit from "next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-});
+// import withPWAInit from "next-pwa";
+//
+// const withPWA = withPWAInit({
+//   dest: "public",
+// });
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   reactStrictMode: true,
   reactCompiler: true,
 };
 
-export default {
-  reactStrictMode: true,
-};
+export default nextConfig;
 
 //export default withPWA(nextConfig);
